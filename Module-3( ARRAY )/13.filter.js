13515d5dcb059663a23f14dd7842039e006738b1
Parth Nagar <parthnagar2406@gmail.com>
2025-08-16 20:36:45 +0530

    Create 13.filter.js

---
+++ Module-3( ARRAY )/13.filter.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5, 6]; 
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers); // Output: [2, 4, 6]
+
+// creates a new array with all elements that pass the test implemented by the provided function
