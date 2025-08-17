89be9e6029c10b6c92dd140ee0d91554fd49154f
Parth Nagar <parthnagar2406@gmail.com>
2025-08-17 15:53:39 +0530

    Update 5.(Async Await & Promise.all).js

--- Module-9(Advanced JS Topics)/5.(Async Await & Promise.all).js
+++ Module-9(Advanced JS Topics)/5.(Async Await & Promise.all).js
@@ -12,3 +12,8 @@ async function fetchData() {
 
 fetchData();
 //Promise.all runs multiple promises in parallel:
+
+const p1 = Promise.resolve(1);
+const p2 = Promise.resolve(2);
+
+Promise.all([p1, p2]).then(results => console.log(results)); // [1, 2]
